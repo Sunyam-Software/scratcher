e1b314460d86a8bee6ec3233468536dfc222383e
aniruddha-sunyam <aniruddha@youandimpact.com>
2024-03-15 17:29:28 +0530

    checking

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -139,6 +139,7 @@ class ScratcherState extends State<Scratcher> {
     return Stack(
   children: [
     Listener(
+      key: GlobalKey(),
       onPointerDown: canScratch
         ? (details) {
           widget.onScratchStart?.call();
