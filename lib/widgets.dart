dfd463b47ccbd3427e715f362233fc2a94736f25
aniruddha-sunyam <aniruddha@youandimpact.com>
2024-03-18 12:55:00 +0530

    checking

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -139,6 +139,7 @@ class ScratcherState extends State<Scratcher> {
     return Stack(
   children: [
     Listener(
+      behavior: HitTestBehavior.translucent,
       onPointerDown: canScratch
         ? (details) {
           widget.onScratchStart?.call();
